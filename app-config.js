091cec92f376899e0ca889f590c671e0a7adb8d2
MarshallRJ <rjmarshall@remotemetering.net>
2017-10-03 15:30:48 +0200

    [config] add new go.cd rms user and password

--- app-config.js
+++ app-config.js
@@ -8,9 +8,9 @@ var config = {
     // Url for your go server
     goServerUrl: 'http://192.168.0.176:8153',
     // Go user to use for communication with go server
-    goUser: undefined,
+    goUser: 'rms',
     // Password for go user
-    goPassword: undefined,
+    goPassword: 'rms',
     // How often data from go should be refreshed in seconds
     goPollingInterval: 30,
     // If > 0 switches between pipeline and test results page every n seconds
